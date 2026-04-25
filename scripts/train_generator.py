6c6647d1f3a3ffd19821a53210b5292a9b35b308
VuTrinhNguyenHoang <trinhnguyenhoangvu306@gmail.com>
2026-04-26 04:04:03 +0700

    fix: tokenizer param has been deprecated

--- scripts/train_generator.py
+++ scripts/train_generator.py
@@ -48,7 +48,7 @@ def main():
         args=training_args,
         train_dataset=train_dataset,
         eval_dataset=valid_dataset,
-        tokenizer=tokenizer,
+        processing_class=tokenizer,
         data_collator=data_collator,
     )
 
